74a45d6583ab2496710b3b123517ea2e52e13d99
Christopher Wilson <cjwilson2020@gmail.com>
2020-10-22 15:09:05 -0500

    Connected realtime database, auth, and storage, sdks. Added a write to realtime database in MainActivity.java's OnCreate method

--- app/src/main/java/com/example/whatdoyouwannawatch/MainActivity.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/MainActivity.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        myRef.setValue("Hello, World!");
 
     }
 
