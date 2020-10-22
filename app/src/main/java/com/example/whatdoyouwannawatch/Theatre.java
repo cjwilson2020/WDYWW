e4b6523fee296bc9d4f4e96fdf43a8accc152921
peterdaly13 <pndaly@wisc.edu>
2020-10-22 14:35:51 -0500

    Updated Theatre and BackStage

--- app/src/main/java/com/example/whatdoyouwannawatch/Theatre.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/Theatre.java
@@ -19,8 +19,118 @@ public class Theatre {
     public Theatre(){
 
     }
-    public Theatre(String uid){
-
+    public Theatre(String uid, int roomNumber, Time timeCreated ){
+        this.uid =uid;
+        this.roomNumber =roomNumber;
+        this.timeCreated = timeCreated;
     }
 
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public BackStage getBackstage() {
+        return backstage;
+    }
+
+    public void setBackstage() {
+        this.backstage = new BackStage(this);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User u){
+        if(!this.users.contains(u)) {
+            this.users.add(u);
+        }
+    }
+
+    public void removeUser(User u){
+        this.users.remove(u);
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public void addGenre(String s){
+        if(!this.genres.contains(s)) {
+            this.genres.add(s);
+        }
+    }
+    public void removeGenre(String s){
+        this.genres.remove(s);
+    }
+
+    public List<Media> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Media> options) {
+        this.options = options;
+    }
+
+    public void addOption(Media m){
+        if(!this.options.contains(m)) {
+            this.options.add(m);
+        }
+    }
+
+    public void removeOption(Media m){
+        this.options.remove(m);
+    }
+
+    public Time getMinTime() {
+        return minTime;
+    }
+
+    public void setMinTime() {
+        this.minTime = backstage.calcLength().get(0);
+    }
+
+    public Time getMaxTime() {
+        return maxTime;
+    }
+
+    public void setMaxTime() {
+        this.maxTime = backstage.calcLength().get(1);
+    }
+
+    public Time getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Time timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
 }
--- app/src/main/java/com/example/whatdoyouwannawatch/User.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/User.java
@@ -1,4 +1,14 @@
 package com.example.whatdoyouwannawatch;
 
 public class User {
+    //Params
+    private Integer [] orderings = {1,2,3,4,5,6,7,8};
+
+
+    //Constructors
+
+
+
+
+    //Getters and Setters
 }
