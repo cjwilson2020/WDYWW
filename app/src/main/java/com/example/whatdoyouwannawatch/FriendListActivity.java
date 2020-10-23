349332acfbf3d73d15efb53f8e69d2c889c0cf48
Meghan Whitehead <mwhitehead2@wisc.edu>
2020-10-23 14:40:45 -0500

    Added more frontend pages

---
+++ app/src/main/java/com/example/whatdoyouwannawatch/FriendListActivity.java
@@ -0,0 +1,14 @@
+package com.example.whatdoyouwannawatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FriendListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+    }
+}
