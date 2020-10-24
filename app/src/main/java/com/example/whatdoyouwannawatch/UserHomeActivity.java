0bfc978e3d4ecfb5e1837d597d28eb5b59024e4f
Christopher Wilson <cjwilson2020@gmail.com>
2020-10-24 13:58:12 -0500

    Added Callback function to MainActivity, reference to it in UserHomeActivity, and the interface class DataCallback

--- app/src/main/java/com/example/whatdoyouwannawatch/UserHomeActivity.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/UserHomeActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import sun.applet.Main;
+
 public class UserHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        // getting user from Firebase
+        // 4. Use that reference to invoke the callback method.
+        MainActivity.pullData(new DataCallBack()  //this is the reference to the callback with the
+                                      // onCallback method implemented to print the returned user
+        {   @Override
+            public void onCallback(User user) {
+                // Here where we can read and edit info from the user object
+               System.out.println(user.toString());
+
+            }
+        }
+        , "1234");
     }
 }
