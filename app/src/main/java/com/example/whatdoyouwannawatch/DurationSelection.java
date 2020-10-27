c78204bae5db30ef77f243551936436c8d2b8464
Yuheng Chen <ccyyhh97@hotmail.com>
2020-10-27 12:37:33 -0500

    DurationSelection added

---
+++ app/src/main/java/com/example/whatdoyouwannawatch/DurationSelection.java
@@ -0,0 +1,14 @@
+package com.example.whatdoyouwannawatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DurationSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duration_selection);
+    }
+}
