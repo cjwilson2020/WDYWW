ce76cefffd0039cd22ef571d7d444a6bda8a5ec9
Meghan Whitehead <mwhitehead2@wisc.edu>
2020-10-30 11:13:26 -0500

    trying to pull data in JoinTheatre

--- app/src/main/java/com/example/whatdoyouwannawatch/JoinTheatre.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/JoinTheatre.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
 
 public class JoinTheatre extends AppCompatActivity {
 
@@ -21,7 +27,25 @@ public class JoinTheatre extends AppCompatActivity {
         EditText editTextTheatreCode = (EditText) findViewById(R.id.editTextNumber_theatreCode);
         String code = editTextTheatreCode.getText().toString(); // change to int
         Intent intent = new Intent(JoinTheatre.this, TheatreUserLandingPage.class);
+        // startActivity(intent);
+        // Toast.makeText(this, "Code entered: " + code, Toast.LENGTH_SHORT).show();
+
+
+
+        MainActivity.pullData('t', code, new DataCallback() {
+            @Override
+            public void onCallback(Object obj) {
+                final FirebaseUser FBuser = FirebaseAuth.getInstance().getCurrentUser();
+                final String uid = FBuser.getUid();
+
+                Theatre theatre = (Theatre) obj;
+                List users = theatre.getUsers();
+                users.add(uid);
+
+                MainActivity.pushData(theatre);
+            }
+        });
+
         startActivity(intent);
-       // Toast.makeText(this, "Code entered: " + code, Toast.LENGTH_SHORT).show();
     }
 }
