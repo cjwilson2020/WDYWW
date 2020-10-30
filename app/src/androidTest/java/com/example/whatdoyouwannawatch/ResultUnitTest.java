d1c9e3140544ccb3a2637fdc17ac85b1270da0e5
yuanchenGH <71296283+yuanchenGH@users.noreply.github.com>
2020-10-30 12:05:24 -0500

    Finished ResultUnitTest

--- app/src/androidTest/java/com/example/whatdoyouwannawatch/ResultUnitTest.java
+++ app/src/androidTest/java/com/example/whatdoyouwannawatch/ResultUnitTest.java
@@ -14,16 +14,24 @@ public class ResultUnitTest {
     List<Media> options;
     Date date;
     Result test;
+    User user1;
+    User user2;
+    User user3;
+    Media m1;
+    Media m2;
+    Media m3;
+    Media m4;
+
 
     @Before
     public void setup(){
-        User user1 = new User("1","1","1");
-        User user2 = new User("2","2","2");
-        User user3 = new User("3","3","3");
-        Media m1 = new Media("11");
-        Media m2 = new Media("22");
-        Media m3 = new Media("33");
-        Media m4 = new Media("44");
+        user1 = new User("1","1","1");
+        user2 = new User("2","2","2");
+        user3 = new User("3","3","3");
+        m1 = new Media("11");
+        m2 = new Media("22");
+        m3 = new Media("33");
+        m4 = new Media("44");
         userList = new ArrayList<>();
         userList.add(user1);
         userList.add(user2);
@@ -34,7 +42,26 @@ public class ResultUnitTest {
         options.add(m3);
         options.add(m4);
         test = new Result(m3, userList, options, date);
+    }
 
+    @Test
+    public void TestGetDecision() {
+        assertEquals(test.getFinalDecision(), m3);
+    }
+
+    @Test
+    public void TestGetUsers() {
+        assertEquals(test.getUsers().get(0), user1);
+        assertEquals(test.getUsers().get(1), user2);
+        assertEquals(test.getUsers().get(2), user3);
+    }
+
+    @Test
+    public void TestGetOptions() {
+        assertEquals(test.getOptions().get(0), m1);
+        assertEquals(test.getOptions().get(1), m2);
+        assertEquals(test.getOptions().get(2), m3);
+        assertEquals(test.getOptions().get(3), m4);
     }
 
     @Test
