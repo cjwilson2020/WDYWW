7317205531e33bf4abc10248d8614151bb48b3cd
Meghan Whitehead <mwhitehead2@wisc.edu>
2020-11-10 15:11:27 -0600

    added ContactUsActivity, still needs pictures

--- app/src/main/java/com/example/whatdoyouwannawatch/ContactUsActivity.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/ContactUsActivity.java
@@ -3,6 +3,9 @@ package com.example.whatdoyouwannawatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class ContactUsActivity extends AppCompatActivity {
 
