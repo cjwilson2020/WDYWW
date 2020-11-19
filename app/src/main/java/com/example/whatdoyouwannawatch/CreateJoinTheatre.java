b2fa47fa2d22eaac46719e8634f72fb3a3131cf8
Yuheng Chen <ccyyhh97@hotmail.com>
2020-11-19 14:10:58 -0600

    styling changes 2

--- app/src/main/java/com/example/whatdoyouwannawatch/CreateJoinTheatre.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/CreateJoinTheatre.java
@@ -42,29 +42,11 @@ public class CreateJoinTheatre extends AppCompatActivity {
                     }
                 }
             });
-
-
             Intent intent = new Intent(CreateJoinTheatre.this, TheatreHostLandingPage.class);
             startActivity(intent);
         }
     }
 
-    public void onClickCreateTheatreUtility(View v){
-        String name;
-
-//        MainActivity.pullData('t', "0", new DataCallback() {
-//            @Override
-//            public void onCallback(Object obj) {
-//                Theatre theatre = (Theatre) obj;
-//
-//                Toast.makeText( CreateJoinTheatre.this, theatre.getHostID(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-        Intent toRanking = new Intent(CreateJoinTheatre.this, DurationSelection.class);
-        startActivity(toRanking);
-
-    }
 
     public void onClickJoinTheatre(View v) {
         Intent intent = new Intent(CreateJoinTheatre.this, JoinTheatre.class);
