6c9b25acda1e5927a8550167d1e469c7ddcc2b81
Yuheng Chen <ccyyhh97@hotmail.com>
2020-12-09 04:54:57 +0800

    refined host flow from CreateJoinTheatre -> TheatreHostLandingPage -> TheatreUserLandingPage -> ChooseGenre to CreateJoinTheatre -> TheatreHostLandingPage  -> ChooseGenre

--- app/src/main/java/com/example/whatdoyouwannawatch/TheatreHostLandingPage.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/TheatreHostLandingPage.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.List;
+
 public class TheatreHostLandingPage extends AppCompatActivity {
     private FirebaseUser fbUser;
-    Button qButton;
-    TextView qTextView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView listView;
+//    private String theatreCode;
 
 
     @Override
@@ -23,12 +30,49 @@ public class TheatreHostLandingPage extends AppCompatActivity {
         setContentView(R.layout.activity_theatre_host_landing_page);
 
         fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        MainActivity.pullData('t', fbUser.getDisplayName(), new DataCallback() {
+            @Override
+            public void onCallback(Object obj) {
+                if(obj!=null) {
+                    Log.i("Null", "Not null");
+                    Theatre t = (Theatre)obj;
+                    final List<User> uList = t.getUsers();
+                    int x = uList.size();
+                    String [] users = new String [x];
+                    for(int i = 0; i< uList.size(); i++){
+                        users[i] = uList.get(i).getUsername();
+                        Log.i("Ulist",users[i]);
+                    }
+                    arrayAdapter = new ArrayAdapter<String>(TheatreHostLandingPage.this, android.R.layout.simple_list_item_1, users);
+                    listView = findViewById(R.id.listView_displayUsersInHost);
+                    listView.setAdapter(arrayAdapter);
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View v, int i, long l) {
+                            onClickFriend(uList, i);
+                        }
+                    });
+                }
+                else{
+                    Log.i("Null", "Null");
+                }
+
+            }
+        });
         displayTheatreID();
+
+//
+    }
+
+    private void onClickFriend(List<User> uList, int i) {
+        Intent intent = new Intent(TheatreHostLandingPage.this, FriendProfileActivity.class);
+        intent.putExtra("username", uList.get(i).getUsername());
+        startActivity(intent);
     }
 
 
     public void onClickNextHost(View v) {
-        Intent intent = new Intent(this, TheatreUserLandingPage.class);
+        Intent intent = new Intent(this, ChooseGenresActivity.class);
         intent.putExtra("theatreCode", fbUser.getDisplayName());
         startActivity(intent);
     }
@@ -55,6 +99,7 @@ public class TheatreHostLandingPage extends AppCompatActivity {
                     Theatre currTheatre = (Theatre) theatre;
                     TextView displayTheatreID = (TextView) findViewById(R.id.textView_displayTheatreID);
                     displayTheatreID.setText("You are in " + currTheatre.getHostID() + "'s theatre ");
+//                    theatreCode = currTheatre.getHostID();
                 }
             }
         });
