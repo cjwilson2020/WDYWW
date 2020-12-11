111f5eb14173ea66e1faf28f2e45ee9449547df9
cjwilson2020 <cjwilson2020@gmail.com>
2020-12-10 20:30:07 -0600

    Quick fix

--- app/src/main/java/com/example/whatdoyouwannawatch/ResultActivity.java
+++ app/src/main/java/com/example/whatdoyouwannawatch/ResultActivity.java
@@ -149,7 +149,7 @@ public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        cntRanked = 0;
+        cntRanked = 1;
         allResult = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
